de4e0ad30a250cefa0dab89f23b66061392d9158
Alexey <taler2002@gmail.com>
2018-10-01 02:19:12 +0300

    added: show one or list of objects

--- src/main/java/com/sypchenko/aleksey/blogapi/controller/ArticleController.java
+++ src/main/java/com/sypchenko/aleksey/blogapi/controller/ArticleController.java
@@ -2,7 +2,9 @@ package com.sypchenko.aleksey.blogapi.controller;
 
 import com.sypchenko.aleksey.blogapi.model.Article;
 import com.sypchenko.aleksey.blogapi.service.ArticleService;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,10 +16,23 @@ public class ArticleController {
     public ArticleController(ArticleService articleService) {
         this.articleService = articleService;
     }
-    @RequestMapping("/articles")
-    public List<Article> showArticlesList() {
+
+    @GetMapping("/articles")
+    public List<Article> getAll() {
         List<Article> articles = articleService.getAllArticle();
         return articles;
     }
 
+    @GetMapping("/articles/{id}")
+    public Article getOne(@PathVariable("id") Article article) {
+        return article;
+    }
+
+    //TODO - сделать добавление статьи
+    //TODO - сделать удаление статьи
+
+    @PostMapping("")
+    public Article add() {
+        return null;
+    }
 }
