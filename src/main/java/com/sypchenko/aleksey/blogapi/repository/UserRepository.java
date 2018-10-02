7f0d467f26dd90d0f1da90846c6621a79c3b6a6b
Alexey <taler2002@gmail.com>
2018-10-02 16:13:14 +0300

    check point

--- src/main/java/com/sypchenko/aleksey/blogapi/repository/UserRepository.java
+++ src/main/java/com/sypchenko/aleksey/blogapi/repository/UserRepository.java
@@ -5,6 +5,9 @@ import com.sypchenko.aleksey.blogapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
 }
