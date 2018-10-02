46fbd3232e0dd0ac9ced5903623e047b9a3fc7a9
Alexey <taler2002@gmail.com>
2018-10-02 18:59:20 +0300

    check point

--- src/main/java/com/sypchenko/aleksey/blogapi/controller/SignUpController.java
+++ src/main/java/com/sypchenko/aleksey/blogapi/controller/SignUpController.java
@@ -1,6 +1,5 @@
 package com.sypchenko.aleksey.blogapi.controller;
 
-import com.sypchenko.aleksey.blogapi.model.UserForm;
 import com.sypchenko.aleksey.blogapi.service.SignUpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +7,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class SignUpController {
-    final
-    SignUpService signUpService;
+    final SignUpService signUpService;
 
     @Autowired
     public SignUpController(SignUpService signUpService) {
@@ -19,17 +19,14 @@ public class SignUpController {
     }
 
     @GetMapping("/signUp")
-    public UserForm getSignUpPage() {
-        return new UserForm();
+    public void getSignUpPage() {
     }
 
     @PostMapping("/signUp")
-    public String signUp(UserForm userForm, @RequestParam("login") String login, @RequestParam("password") String password) {
+    public String signUp(@RequestParam("login") String login, @RequestParam("password") String password) {
         //TODO - проверка на пустоту и длину значений
-        userForm.setLogin(login);
-        userForm.setPassword(password);
-        signUpService.signUp(userForm);
-        //return "redirect:/login";
-        return "signUp POST did";
+        signUpService.signUp(login, password, new Date());
+
+        return "redirect:/login";
     }
 }
